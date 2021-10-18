165f77735daeb3e1b108030ffa2f4a79d077e622
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-18 22:13:47 +0100

    Practice style attributes and classes

--- S13-advanced-dom-and-events/script.js
+++ S13-advanced-dom-and-events/script.js
@@ -33,8 +33,7 @@ document.addEventListener("keydown", function(e) {
     }
 });
 
-// Selecting Elements
-
+// ### Selecting Elements ###
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -49,8 +48,7 @@ console.log(allBtns);
 
 console.log(document.getElementsByClassName("btn"));
 
-// Creating and inserting elements
-
+// ### Creating and inserting elements ###
 const message = document.createElement("div");
 message.classList.add("cookie-message");
 // message.textContent = "We use cookies for improved functionality and analytics";
@@ -67,3 +65,46 @@ header.append(message);
 document
     .querySelector(".btn--close-cookie")
     .addEventListener("click", () => message.remove());
+
+// ### Styles ###
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+    Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// ### Attributes ###
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.src);
+
+logo.alt = "Beautiful minimalist logo";
+
+// ### Non-standard ###
+console.log(logo.degigner); // Undefined
+console.log(logo.getAttribute("designer"));
+logo.setAttribute("Company", "Bankist");
+
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+const link = document.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
+
+//### Data attributes ###
+console.log(logo.dataset.versionNumber);
+
+// ### Classes ###
+logo.classList.add("c", "j");
+logo.classList.remove("c");
+logo.classList.toggle("c");
+logo.classList.contains("c");
+
+logo.className = "jonas"; // Do not use as it replaces all classes
