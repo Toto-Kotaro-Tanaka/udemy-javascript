f551b77abc425dcbef32786a8a0b1ac368fe7bd3
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-19 20:37:06 +0100

    Event progation in practice

--- S13-advanced-dom-and-events/script.js
+++ S13-advanced-dom-and-events/script.js
@@ -145,6 +145,34 @@ h1.addEventListener("mouseenter", (e) => {
     alert("addEventLister: Great! You are reading the heading");
 });
 
-h1.onmouseenter = function(e) {
-    alert("onmouseenter: Great! You are reading the heading");
-};
+// Below old way
+// h1.onmouseenter = function(e) {
+//     alert("onmouseenter: Great! You are reading the heading");
+// };
+
+// rgb 0 - 255
+const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+    `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector(".nav__link").addEventListener("click", function(e) {
+    this.style.backgroundColor = randomColor();
+    console.log("Link", e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+
+    // Stop propagation
+    // e.stopPropagation(); // Usually not a good idea to use it
+});
+
+document.querySelector(".nav__links").addEventListener("click", function(e) {
+    this.style.backgroundColor = randomColor();
+    console.log("Container", e.target, e.currentTarget);
+});
+
+document.querySelector(".nav").addEventListener("click", function(e) {
+    this.style.backgroundColor = randomColor();
+    console.log("Nav", e.target, e.currentTarget);
+});
