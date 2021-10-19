4dc7d3211da33a6ba27d34c4c5fdb9c904ba6ce4
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-19 21:11:59 +0100

    Event delegation

--- S13-advanced-dom-and-events/script.js
+++ S13-advanced-dom-and-events/script.js
@@ -1,12 +1,14 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+///////////////////////////////////////
+// ### Modal window ###
 
 const openModal = function(e) {
     e.preventDefault();
@@ -30,6 +32,63 @@ overlay.addEventListener("click", closeModal);
 document.addEventListener("keydown", function(e) {
     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
         closeModal();
+    }
+});
+
+// ### Button Scrolling ###
+btnScrollTo.addEventListener("click", (e) => {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
+    console.log(e.target.getBoundingClientRect());
+    console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+    console.log(
+        "height/width viewport",
+        document.documentElement.clientHeight,
+        document.documentElement.clientWidth,
+    );
+
+    // Scrolling
+    // window.scrollTo(
+    //     s1coords.left + window.pageXOffset,
+    //     s1coords.top + window.pageYOffset,
+    // );
+
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffset,
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: "smooth",
+    // });
+
+    section1.scrollIntoView({
+        behavior: "smooth",
+    });
+});
+
+// ### Page Navigation ###
+// document.querySelectorAll(".nav__link").forEach(function(el) {
+//     el.addEventListener("click", function(e) {
+//         e.preventDefault();
+//         const id = this.getAttribute("href");
+//         console.log(id);
+//         document.querySelector(id).scrollIntoView({
+//             behavior: "smooth",
+//         });
+//     });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains("nav__link")) {
+        const id = e.target.getAttribute("href");
+        console.log(id);
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+        });
     }
 });
 
@@ -109,37 +168,6 @@ logo.classList.contains("c");
 
 logo.className = "jonas"; // Do not use as it replaces all classes
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
-btnScrollTo.addEventListener("click", (e) => {
-    const s1coords = section1.getBoundingClientRect();
-    console.log(s1coords);
-    console.log(e.target.getBoundingClientRect());
-    console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
-    console.log(
-        "height/width viewport",
-        document.documentElement.clientHeight,
-        document.documentElement.clientWidth,
-    );
-
-    // Scrolling
-    // window.scrollTo(
-    //     s1coords.left + window.pageXOffset,
-    //     s1coords.top + window.pageYOffset,
-    // );
-
-    // window.scrollTo({
-    //     left: s1coords.left + window.pageXOffset,
-    //     top: s1coords.top + window.pageYOffset,
-    //     behavior: "smooth",
-    // });
-
-    section1.scrollIntoView({
-        behavior: "smooth",
-    });
-});
-
 const h1 = document.querySelector("h1");
 h1.addEventListener("mouseenter", (e) => {
     alert("addEventLister: Great! You are reading the heading");
@@ -158,21 +186,21 @@ const randomColor = () =>
     `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 console.log(randomColor(0, 255));
 
-document.querySelector(".nav__link").addEventListener("click", function(e) {
-    this.style.backgroundColor = randomColor();
-    console.log("Link", e.target, e.currentTarget);
-    console.log(e.currentTarget === this);
+// document.querySelector(".nav__link").addEventListener("click", function(e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log("Link", e.target, e.currentTarget);
+//     console.log(e.currentTarget === this);
 
-    // Stop propagation
-    // e.stopPropagation(); // Usually not a good idea to use it
-});
+// Stop propagation
+// e.stopPropagation(); // Usually not a good idea to use it
+// });
 
-document.querySelector(".nav__links").addEventListener("click", function(e) {
-    this.style.backgroundColor = randomColor();
-    console.log("Container", e.target, e.currentTarget);
-});
+// document.querySelector(".nav__links").addEventListener("click", function(e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log("Container", e.target, e.currentTarget);
+// });
 
-document.querySelector(".nav").addEventListener("click", function(e) {
-    this.style.backgroundColor = randomColor();
-    console.log("Nav", e.target, e.currentTarget);
-});
+// document.querySelector(".nav").addEventListener("click", function(e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log("Nav", e.target, e.currentTarget);
+// });
