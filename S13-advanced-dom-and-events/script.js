b85244463f8bffa7bbfe780caae75db30e35d6a9
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-22 18:27:47 +0100

    DOM traversing

--- S13-advanced-dom-and-events/script.js
+++ S13-advanced-dom-and-events/script.js
@@ -168,10 +168,10 @@ logo.classList.contains("c");
 
 logo.className = "jonas"; // Do not use as it replaces all classes
 
-const h1 = document.querySelector("h1");
-h1.addEventListener("mouseenter", (e) => {
-    alert("addEventLister: Great! You are reading the heading");
-});
+// const h1 = document.querySelector("h1");
+// h1.addEventListener("mouseenter", (e) => {
+//     alert("addEventLister: Great! You are reading the heading");
+// });
 
 // Below old way
 // h1.onmouseenter = function(e) {
@@ -204,3 +204,31 @@ console.log(randomColor(0, 255));
 //     this.style.backgroundColor = randomColor();
 //     console.log("Nav", e.target, e.currentTarget);
 // });
+
+const h1 = document.querySelector("h1");
+
+// Going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.children);
+
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+// Going upwards: parent
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+// Going sideways: siblings
+console.log(h1.previousElementsSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach((el) => {
+    if (el !== h1) el.style.transform = "scale(0.5)";
+});
