d07b4ccab1da4b357c9a4568889aa789c20fa4e3
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-23 15:03:35 +0100

    Implement a sticky navigation scroll event

--- S13-advanced-dom-and-events/script.js
+++ S13-advanced-dom-and-events/script.js
@@ -241,6 +241,19 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 // });
 nav.addEventListener("mouseout", handleHover.bind(1));
 
+// ### Sticky Navigation ###
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener("scroll", function() {
+    // console.log(window.scrollY);
+
+    if (window.scrollY > initialCoords.top) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+});
+
 // document.querySelector(".nav__link").addEventListener("click", function(e) {
 //     this.style.backgroundColor = randomColor();
 //     console.log("Link", e.target, e.currentTarget);
