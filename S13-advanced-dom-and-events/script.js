596763479d4e64dff8f9eafaab0ae3a578ef02ee
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-24 18:48:56 +0100

    Add slider component part 1

--- S13-advanced-dom-and-events/script.js
+++ S13-advanced-dom-and-events/script.js
@@ -15,10 +15,6 @@ const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
 const nav = document.querySelector(".nav");
 const imgTarget = document.querySelectorAll("img[data-src]");
-const slides = document.querySelectorAll(".slide");
-const slider = document.querySelector(".slider");
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
 
 ///////////////////////////////////////
 // ### Modal Window ###
@@ -341,39 +337,90 @@ const imagObserver = new IntersectionObserver(loadImg, {
 imgTarget.forEach((img) => imagObserver.observe(img));
 
 // ### Slider ###
-let currentSlide = 0;
-const maxSlide = slides.length;
+const slider = function() {
+    const slides = document.querySelectorAll(".slide");
+    const slider = document.querySelector(".slider");
+    const btnLeft = document.querySelector(".slider__btn--left");
+    const btnRight = document.querySelector(".slider__btn--right");
+    const dotContainer = document.querySelector(".dots");
 
-const goToSlide = function(slide) {
-    slides.forEach(
-        (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`),
-    );
+    let currentSlide = 0;
+    const maxSlide = slides.length;
+
+    const createDots = function() {
+        slides.forEach((_, i) => {
+            dotContainer.insertAdjacentHTML(
+                "beforeend",
+                `<button class="dots__dot" data-slide="${i}"></button>`,
+            );
+        });
+    };
+
+    const activateDot = function(slide) {
+        document
+            .querySelectorAll(".dots__dot")
+            .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+        document
+            .querySelector(`.dots__dot[data-slide="${slide}"]`)
+            .classList.add("dots__dot--active");
+    };
+
+    const goToSlide = function(slide) {
+        slides.forEach(
+            (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`),
+        );
+    };
+
+    const nextSlide = () => {
+        if (currentSlide === maxSlide - 1) {
+            currentSlide = 0;
+        } else {
+            currentSlide++;
+        }
+
+        goToSlide(currentSlide);
+        activateDot(currentSlide);
+    };
+
+    const prevSlide = () => {
+        if (currentSlide === 0) {
+            currentSlide = maxSlide - 1;
+        } else {
+            currentSlide--;
+        }
+
+        goToSlide(currentSlide);
+        activateDot(currentSlide);
+    };
+
+    const init = function() {
+        createDots();
+        activateDot(0);
+        goToSlide(0);
+    };
+
+    init();
+
+    // Event Handlers
+    btnRight.addEventListener("click", nextSlide);
+    btnLeft.addEventListener("click", prevSlide);
+
+    document.addEventListener("keydown", function(e) {
+        if (e.key === "ArrowLeft") prevSlide();
+        e.key === "ArrowRight" && nextSlide();
+    });
+
+    dotContainer.addEventListener("click", function(e) {
+        if (e.target.classList.contains("dots__dot")) {
+            const { slide } = e.target.dataset;
+            goToSlide(slide);
+            activateDot(slide);
+        }
+    });
 };
 
-goToSlide(0);
-
-const nextSlide = () => {
-    if (currentSlide === maxSlide - 1) {
-        currentSlide = 0;
-    } else {
-        currentSlide++;
-    }
-
-    goToSlide(currentSlide);
-};
-
-const prevSlide = () => {
-    if (currentSlide === 0) {
-        currentSlide = maxSlide - 1;
-    } else {
-        currentSlide--;
-    }
-
-    goToSlide(currentSlide);
-};
-
-btnRight.addEventListener("click", nextSlide);
-btnLeft.addEventListener("click", prevSlide);
+slider();
 
 // document.querySelector(".nav__link").addEventListener("click", function(e) {
 //     this.style.backgroundColor = randomColor();
