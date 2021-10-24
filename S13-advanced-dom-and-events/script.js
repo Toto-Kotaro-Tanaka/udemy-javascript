bdd8079c46c8ac03e23b9e3f31cc22979e5f1e57
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-10-24 17:04:07 +0100

    Add lazy loading imgs

--- S13-advanced-dom-and-events/script.js
+++ S13-advanced-dom-and-events/script.js
@@ -14,6 +14,7 @@ const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
 const nav = document.querySelector(".nav");
+const imgTarget = document.querySelectorAll("img[data-src]");
 
 ///////////////////////////////////////
 // ### Modal Window ###
@@ -297,7 +298,7 @@ const revealSection = function(entries, observer) {
     if (!entry.isIntersecting) return;
 
     entry.target.classList.remove("section--hidden");
-    observe.unobserve(entry.target);
+    observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -309,6 +310,31 @@ allSections.forEach(function(section) {
     sectionObserver.observe(section);
     section.classList.add("section--hidden");
 });
+
+// ### Lazy Loading Images ###
+
+const loadImg = (entries, observer) => {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener("load", function() {
+        entry.target.classList.remove("lazy-img");
+    });
+
+    observer.unobserve(entry.target);
+};
+
+const imagObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: "-200px",
+});
+
+imgTarget.forEach((img) => imagObserver.observe(img));
 
 // document.querySelector(".nav__link").addEventListener("click", function(e) {
 //     this.style.backgroundColor = randomColor();
