551d5c95aece1111b772ac39b791bfb1a9377436
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-11-11 15:59:04 +0000

    Error handling with try and catch

--- s16-async/script3.js
+++ s16-async/script3.js
@@ -6,22 +6,44 @@ const getPosition = function () {
 };
 
 const whereAmI = async function () {
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
+    try {
+        const pos = await getPosition();
+        const { latitude: lat, longitude: lng } = pos.coords;
 
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    const dataGeo = await resGeo.json();
-    console.log(dataGeo);
+        const resGeo = await fetch(
+            `https://geocode.xyz/${lat},${lng}?geoit=json`,
+        );
 
-    // fetch(`https://restcountries.com/v2/name/${country}`).then((res) =>
-    //     console.log(res),
-    // ); // Below is same as this
-    const res = await fetch(
-        `https://restcountries.com/v2/name/${dataGeo.country}`,
-    );
-    const data = await res.json();
-    console.log(data);
+        if (!resGeo.ok) throw new Error(`Problem getting location data`);
+        const dataGeo = await resGeo.json();
+        console.log(dataGeo);
+
+        // fetch(`https://restcountries.com/v2/name/${country}`).then((res) =>
+        //     console.log(res),
+        // ); // Below is same as this
+        const res = await fetch(
+            `https://restcountries.com/v2/name/${dataGeo.country}`,
+        );
+
+        if (!res.ok) throw new Error(`Problem getting country`);
+        const data = await res.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err.message);
+        renderError(`Something went wrong ${err.message}`);
+    }
 };
 
 whereAmI();
+whereAmI();
+whereAmI();
+whereAmI();
 console.log("this is first");
+
+// try {
+//     let y = 1;
+//     const x = 2;
+//     y = 3;
+// } catch (err) {
+//     alert(err.message);
+// }
