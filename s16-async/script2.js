f8bb3f9f536a1f6bc51be3c8bea72ae80eb8ea83
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-11-12 19:54:25 +0000

    Coding challenge 3 part 1

--- s16-async/script2.js
+++ s16-async/script2.js
@@ -109,6 +109,52 @@ const whereAmI = function () {
 btn.addEventListener("click", whereAmI);
 
 // Coding Challenge 2
+// const wait = function (seconds) {
+//     return new Promise(function (resolve) {
+//         setTimeout(resolve, seconds * 1000);
+//     });
+// };
+
+// const imgContainer = document.querySelector(".images");
+
+// const createImage = function (imgPath) {
+//     return new Promise(function (resolve, reject) {
+//         const img = document.createElement("img");
+//         img.src = imgPath;
+
+//         img.addEventListener("load", function () {
+//             imgContainer.append(img);
+//             resolve(img);
+//         });
+
+//         img.addEventListener("error", function () {
+//             reject(new Error("Image not loaded"));
+//         });
+//     });
+// };
+
+// let currentImg;
+// createImage("./img/img-1.jpg")
+//     .then((img) => {
+//         currentImg = img;
+//         console.log("Image 1 loaded");
+//         return wait(2);
+//     })
+//     .then(() => {
+//         currentImg.style.display = "none";
+//         return createImage("./img/img-2.jpg");
+//     })
+//     .then((img) => {
+//         currentImg = img;
+//         console.log("Image 2 loaded");
+//         return wait(2);
+//     })
+//     .then(() => {
+//         currentImg.style.display = "none";
+//     })
+//     .catch((err) => console.error(err.message));
+
+// Coding Challenge 3
 const wait = function (seconds) {
     return new Promise(function (resolve) {
         setTimeout(resolve, seconds * 1000);
@@ -133,23 +179,24 @@ const createImage = function (imgPath) {
     });
 };
 
-let currentImg;
-createImage("./img/img-1.jpg")
-    .then((img) => {
-        currentImg = img;
-        console.log("Image 1 loaded");
-        return wait(2);
-    })
-    .then(() => {
-        currentImg.style.display = "none";
-        return createImage("./img/img-2.jpg");
-    })
-    .then((img) => {
-        currentImg = img;
-        console.log("Image 2 loaded");
-        return wait(2);
-    })
-    .then(() => {
-        currentImg.style.display = "none";
-    })
-    .catch((err) => console.error(err.message));
+const loadNPause = async function () {
+    try {
+        // Display image 1
+        let img = await createImage("./img/img-1.jpg");
+        console.log(`Image 1 loaded`);
+
+        await wait(2);
+        img.style.display = "none";
+
+        // Display image 2
+        img = await createImage("./img/img-2.jpg");
+        console.log(`Image 2 loaded`);
+
+        await wait(2);
+        img.style.display = "none";
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
+loadNPause();
