e9b8561753aafc4470a5322efb9f6dc67b4d36a2
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-11-13 21:12:49 +0000

    Exporting and importing in ES6 modules

--- s17-modern-js/script.js
+++ s17-modern-js/script.js
@@ -1,3 +1,22 @@
 // Importing Module
-import "./shoppingCart.js";
+// import {
+//     addToCart,
+//     totalPrice as price, // To change the name
+//     tq,
+// } from "./shoppingCart.js";
+
+// addToCart("Bread", 5);
+// console.log(price, tq);
 console.log("Importing module");
+
+// import * as ShoppingCart from "./shoppingCart.js"; // To import everything
+
+// ShoppingCart.addToCart("tomato", 23);
+// console.log(ShoppingCart.totalPrice);
+
+import add, { cart } from "./shoppingCart.js";
+add("pizza", 3);
+add("apple", 56);
+add("bread", 9);
+
+console.log(cart); // Import is not a copy of export, that's why there are objects in the array
--- s17-modern-js/shoppingCart.js
+++ s17-modern-js/shoppingCart.js
@@ -1,2 +1,20 @@
 // Exporting Module
 console.log("Exporting Module");
+
+const shippingCost = 10;
+export const cart = [];
+
+export const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added`);
+};
+
+const totalPrice = 235;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq }; // As tq is to change the variable name on export
+
+export default function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added`);
+}
