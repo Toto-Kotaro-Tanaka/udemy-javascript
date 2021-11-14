2cdb3aa971eb685c1349284722be99869bbe344b
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-11-14 16:30:56 +0000

    The module pattern

--- s17-modern-js/script.js
+++ s17-modern-js/script.js
@@ -42,3 +42,33 @@ console.log(lastPost);
 
 // const lastPost2 = await getLastPost();
 // console.log(lastPost2);
+
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(
+            `This is shopping cart 2: ${quantity} ${product} added to cart`,
+        );
+    };
+
+    const orderStock = function (product, quantity) {
+        console.log(
+            `This is shopping cart 2: ${quantity} ${product} ordered from supplier`,
+        );
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    };
+})();
+
+ShoppingCart2.addToCart("apple", 2);
+ShoppingCart2.addToCart("pizza", 3);
